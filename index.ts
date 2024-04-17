af321d31d797613142bbd67224b914f86faa6c07
alizaabid <155987307+alizaabid@users.noreply.github.com>
2024-04-17 21:56:44 +0500

    push on github

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer:{
+    sentence:string
+} = await inquirer.prompt([
+    {
+        name:"sentence",
+    type:"input",
+    message:"enter your sentence to count the word:"
+}
+])
+const words = answer.sentence.trim().split("")
+console.log(`your sentence word count is ${words.length}`)
